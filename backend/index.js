40d2fd1cf53b74857cbd633c43e35686b7cb40b1
Serpent03 <dumbpilot03@gmail.com>
2023-10-05 10:39:54 +0530

    feat: express API

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,24 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 
-// require('dotenv').config();
+require('dotenv').config(); // load all the environment variables
+
+const PORT = process.env.PORT || 3001;
+const URI_KEY = "";
+// const 
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(3001, () => {
-  console.log("yahoo");
+// ADD ROUTES
+// ADD ERROR HANDLING
+// ADD MONGOOSE UTILITY
+
+const test = require("./routes/test.js");
+
+app.use("/test", test);
+
+app.listen(PORT, () => {
+  console.log(`yahoo: ${PORT}`);
 })
 
