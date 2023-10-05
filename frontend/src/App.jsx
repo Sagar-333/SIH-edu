2b9e3b5d5b22e2355841b0777112a5ea5bb5cd1f
Sagar-333 <sagarkhanna030303@gmail.com>
2023-10-05 11:26:17 +0530

    home ini

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import {Routes, Route} from "react-router-dom"
 import Register from './Pages/Register/register'
 import Login from './Pages/Login/login'
+import Home from './Pages/Home/home'
 function App() {
   return (
     <div>
       <Routes>
+        <Route path='/' element={<Home/>}/>
         <Route path="/login" element={<Login/>} /> 
         <Route path="/register" element={<Register/>} />
       </Routes>
