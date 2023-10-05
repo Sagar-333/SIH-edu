6f06e1fe8bc6bf4c7f61f82b3526bd356b4da69d
Serpent03 <dumbpilot03@gmail.com>
2023-10-05 16:16:35 +0530

    feat: authentication middleware

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,15 @@ app.use(cors());
 // ADD ROUTES
 // ADD ERROR HANDLING
 // ADD MONGOOSE UTILITY
+// ADD JWT HANDLING
+
+// ROUTES
 
 const test = require("./routes/test.js");
+const auth = require("./routes/auth.js");
 
 app.use("/test", test);
+app.use("/auth", auth);
 
 app.listen(PORT, () => {
   console.log(`yahoo: ${PORT}`);
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,33 +1,19 @@
 const express = require("express");
+const authenticationController = require("../controllers/authenticationController.js");
 const router = express.Router();
 
 // Both <WEBSITE>/auth/register and <WEBSITE>/auth/login will lead to their own
 // non-lambda functions for their own respective job.
 
-router.post("/register", (req, res) => {
-  if (!req.body.password || !req.body.username || !req.body.email) {
-    res.status(405).json({
-      message: "ERROR!"
-    });
-  }
-  res.json({
-    uname: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
-});
+router.post("/register", authenticationController.register);
 
-router.post("/login", (req, res) => {
+router.post("/login", (req, res, next) => {
   if (!req.body.password || !req.body.username) {
     res.status(405).json({
       message: "ERROR!"
     });
   }
-  res.json({
-    uname: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
+  authenticationController.login(req, res, next);
 });
 
 module.exports = router;
