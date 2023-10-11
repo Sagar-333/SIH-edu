60a61e49a1122e66571ce67b1cfb3b586ab22930
Serpent03 <dumbpilot03@gmail.com>
2023-10-11 13:10:56 +0530

    feat: JWT cookie fixes

--- backend/middleware/jwtDecode.js
+++ backend/middleware/jwtDecode.js
@@ -3,8 +3,7 @@ const jwt = require("jsonwebtoken");
 const decodeClientKey = (authorizationToken) => {
   // get the JWT key and confirm whether it is valid or not
   try {
-    const token = authorizationToken.split(" ");
-    return jwt.verify(token[1], process.env.JWT_SECRET).id;
+    return jwt.verify(authorizationToken, process.env.JWT_SECRET).id;
   } catch (error) {
     res.status(412).json({
       message: "Authentication failed!",
@@ -12,4 +11,4 @@ const decodeClientKey = (authorizationToken) => {
   }
 };
 
-exports.decodeClientKey = decodeClientKey;
+module.exports = decodeClientKey;
